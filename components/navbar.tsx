8e0be1a39b5d00bdb059c8b0061addd9fbd36c72
Luben Stoyanov <luben.stoyanov.ls@gmail.com>
2023-04-27 09:44:14 +0200

    Increase margin on x-axis

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,22 +3,22 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <nav className="flex justify-between items-center text-[3vw] md:text-[1vw] py-4 mx-4 bg-white border-b border-black">
-      <Link href="/" className="text-gray-400  hover:text-gray-700">
+      <Link href="/" className="text-gray-500  hover:text-black">
         LUBEN STOYANOV
       </Link>
       <ul className="flex space-x-4">
         <li>
-          <a href="#about" className="text-gray-400 hover:text-gray-700">
+          <a href="#about" className="text-gray-500 hover:text-black">
             ABOUT
           </a>
         </li>
         <li>
-          <a href="#works" className="text-gray-400 hover:text-gray-700">
+          <a href="#works" className="text-gray-500 hover:text-black">
             WORKS
           </a>
         </li>
         <li>
-          <a href="#contact" className="text-gray-400 hover:text-gray-700">
+          <a href="#contact" className="text-gray-500 hover:text-black">
             CONTACT
           </a>
         </li>
