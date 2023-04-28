426e04be41da5fc8fa84005c38391eda1a34527c
Luben Stoyanov <luben.stoyanov.ls@gmail.com>
2023-04-28 17:43:37 +0200

    Type animate hero text

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,37 +1,71 @@
-import { motion } from "framer-motion";
-import * as Scroll from "react-scroll";
+import { TypeAnimation } from "react-type-animation";
+import Typewriter from "typewriter-effect";
 
 export default function Hero() {
   return (
-    <motion.section
-      initial={{ opacity: 0, scale: 0 }}
-      animate={{ opacity: 1, scale: 1 }}
-      transition={{
-        duration: 0.8,
-        delay: 0.5,
-        ease: [0, 0.71, 0.2, 1.01],
-      }}
-      className="min-h-screen sm:min-h-screen"
-    >
-      <h1 className="pt-52  text-[10vw] leading-[10vw] sm:text-[5vw] sm:leading-[5vw]">
+    <section className="min-h-screen sm:min-h-screen">
+      {/* <h1 className="pt-52  text-[10vw] leading-[10vw] sm:text-[5vw] sm:leading-[5vw]">
         HELLO, I&apos;M{" "}
         <a href="#about">
           <motion.em
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}
-            className="text-orange-500"
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          className="text-orange-500"
           >
-            LUBEN
+          LUBEN
           </motion.em>
-        </a>{" "}
-        , A <em className="text-orange-500">WEB DEVELOPER</em> WHO EN
-        <em className="text-orange-500">JOY</em>S BUILDING WEBSITES THAT LOOK{" "}
-        <em className="text-orange-500">GREAT</em> AND PROVIDE A{" "}
-        <em className="text-orange-500">MEANING</em>FUL EXPERIENCE FOR THEIR
-        USERS.
-      </h1>
-    </motion.section>
+          </a>{" "}
+          , A <em className="text-orange-500">WEB DEVELOPER</em> WHO EN
+          <em className="text-orange-500 ">JOY</em>S BUILDING WEBSITES THAT LOOK{" "}
+          <em className="text-orange-500">GREAT</em> AND PROVIDE A{" "}
+          <em className="text-orange-500">MEANING</em>FUL EXPERIENCE FOR THEIR
+          USERS.
+        </h1> */}
+      {/* <TypeAnimation
+        className="pt-52  text-[10vw] leading-[10vw] sm:text-[5vw] sm:leading-[5vw]"
+        sequence={[
+          "HELLO, I'M LUBEN, A WEB DEVELOPER WHO ENJOYS BUILDING WEBSITES THAT LOOK GREAT AND PROVIDE A MEANINGFUL EXPERIENCE FOR THEIR USERS.",
+          () => {
+            console.log("Sequence completed"); // Place optional callbacks anywhere in the array
+          },
+        ]}
+        wrapper="h1"
+        cursor={false}
+      /> */}
+      {/* <TypeAnimation
+        key={123}
+        className="pt-52 text-[10vw] leading-[10vw] sm:text-[5vw] sm:leading-[5vw]"
+        sequence={[
+          "HELLO, I'M LUBEN, A WEB DEVELOPER WHO ENJOYS BUILDING WEBSITES THAT LOOK GREAT AND PROVIDE A MEANINGFUL EXPERIENCE FOR THEIR USER.",
+        ]}
+        wrapper="h1"
+        cursor={false}
+      /> */}
+
+      <div className="pt-52 text-[10vw] leading-[10vw] sm:text-[5vw] sm:leading-[5vw]">
+        <Typewriter
+          onInit={(typewriter) => {
+            typewriter
+              .typeString(
+                "HELLO, I'M <em class='text-orange-500'>LUBEN</em>, A <em class='text-orange-500'>WEB DEVELOPER</em> WHO EN<em class='text-orange-500'>JOY</em>S BUILDING WEBSITES THAT LOOK <em class='text-orange-500'>GREAT</em> AND PROVIDE A <em class='text-orange-500'>MEANING</em>FUL EXPERIENCE FOR THEIR USER."
+              )
+              .start();
+          }}
+        />
+      </div>
+    </section>
   );
 }
 
 //max-w-[20ch]
+
+`HELLO, I&apos;M{" "}
+
+
+            LUBEN
+
+        , A <em className="text-orange-500">WEB DEVELOPER</em> WHO EN
+        <em className="text-orange-500 ">JOY</em>S BUILDING WEBSITES THAT LOOK{" "}
+        <em className="text-orange-500">GREAT</em> AND PROVIDE A{" "}
+        <em className="text-orange-500">MEANING</em>FUL EXPERIENCE FOR THEIR
+        USERS.`;
