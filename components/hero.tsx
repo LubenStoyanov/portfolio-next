684cf0854c77e3a41834c5006ab2c3c878030b3e
Luben Stoyanov <luben.stoyanov.ls@gmail.com>
2023-05-02 06:38:24 +0200

    Git pull needed

--- components/hero.tsx
+++ components/hero.tsx
@@ -6,7 +6,7 @@ export default function Hero({ translate }: { translate: Function }) {
   return (
     <section className="min-h-[240px] text-5xl py-16">
       <div className="min-h-[240px]">
-        <span className="text-xs font-semibold">&lt;h1&gt;</span>{" "}
+        <span className="text-xs font-semibold">&lt;h1&gt;</span>
         <TypewriterComponent hero={hero} />
         <span className="text-xs font-semibold relative bottom-6">
           &lt;/h1&gt;
@@ -16,17 +16,21 @@ export default function Hero({ translate }: { translate: Function }) {
         <span className="text-xs font-semibold relative bottom-[2px]">
           &lt;p&gt;
         </span>
-        I en
-        <em className="text-orange-500 mr-1">joy</em> building{" "}
-        <em className="text-orange-500">great</em> looking websites that provide
-        a <em className="text-orange-500 mr-1">meaning</em>ful experience for
-        their user.
+        {translate("hero.1")}
+        <em className="text-orange-500 mr-1">{translate("hero.joy")}</em>
+        {translate("hero.2")}
+        <em className="text-orange-500">{translate("hero.great")}</em>
+        {translate("hero.3")}
+        <em className="text-orange-500 mr-1">
+          {translate("hero.meaning")}
+        </em>{" "}
+        {translate("hero.4")}
         <span className="text-xs font-semibold relative bottom-[2px]">
           &lt;/p&gt;
         </span>
       </p>
       <button className="text-sm font-bold border-2 border-black hover:border-2 hover:border-orange-500 rounded-3xl px-5 py-3">
-        GET IN TOUCH
+        {translate("hero.cat-button")}
       </button>
     </section>
   );
