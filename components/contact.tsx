667a6915b01bdb96ef2c602584636833030aadeb
Luben Stoyanov <luben.stoyanov.ls@gmail.com>
2023-05-04 13:06:52 +0200

    Make Contact responsive

--- components/contact.tsx
+++ components/contact.tsx
@@ -7,37 +7,38 @@ export default function Contact({ translate }: { translate: Function }) {
       id="contact"
       className="flex flex-col py-16 text-white bg-[#0D0E13]"
     >
-      <div className="contact-wrapper mx-7">
+      <div className=" mx-7 lg:px-80">
         <div className="flex place-items-center place-content-center gap-x-1">
-          <span className="text-sm font-bold">&lt;h2&gt; </span>
+          <span className="text-xs font-bold">&lt;h2&gt; </span>
           <span className="border-t-2 w-20 lg:w-4/12 border-white"></span>
           <h2 className="font-bold lg:text-5xl text-3xl text-center">
             {translate("contact.title")}
           </h2>
           <span className="border-t-2 w-20 lg:w-4/12 border-white"></span>
-          <span className="text-sm font-bold">&lt;h2/&gt;</span>
+          <span className="text-xs font-bold">&lt;h2/&gt;</span>
         </div>
-        <p className="pt-10 max-w-[40ch]">
-          <span className="text-xs font-semibold relative bottom-[2px]">
-            &lt;p&gt;
-          </span>{" "}
-          {translate("contact.1")}
-          <b className="text-[#EF6706]">{translate("contact.passionate")}</b>
-          {translate("contact.2")}
-          <b className="text-[#EF6706]">{translate("contact.valuable")}</b>
-          {translate("contact.3")}
-          <b className="text-[#EF6706]">{translate("contact.workTogether")}</b>
-          {translate("contact.4")}
-          <b className="text-[#EF6706]">{translate("contact.vision")}</b>
-          {translate("contact.5")}{" "}
-          <span className="text-xs font-semibold relative bottom-[2px]">
-            &lt;/p&gt;
-          </span>
-        </p>
-        <p className="text-left text-base text-white font-bold mt-10">
-          {/* {translate("contact.connect")} */}
-        </p>
-        <button className="max-w-fit font-bold">
+        <div className="lg:flex lg:justify-center">
+          <p className="pt-10  lg:max-w-[60ch">
+            <span className="text-xs font-semibold relative bottom-[2px]">
+              &lt;p&gt;
+            </span>{" "}
+            {translate("contact.1")}
+            <b className="text-[#EF6706]">{translate("contact.passionate")}</b>
+            {translate("contact.2")}
+            <b className="text-[#EF6706]">{translate("contact.valuable")}</b>
+            {translate("contact.3")}
+            <b className="text-[#EF6706]">
+              {translate("contact.workTogether")}
+            </b>
+            {translate("contact.4")}
+            <b className="text-[#EF6706]">{translate("contact.vision")}</b>
+            {translate("contact.5")}{" "}
+            <span className="text-xs font-semibold relative bottom-[2px]">
+              &lt;/p&gt;
+            </span>
+          </p>
+        </div>
+        <button className="max-w-fit font-bold mt-10">
           <a
             href="mailto:luben.stoyanov.ls@gmail.com?subject=Inquiry"
             className="no-underline"
@@ -54,6 +55,9 @@ export default function Contact({ translate }: { translate: Function }) {
             </div>
           </a>
         </button>
+        {/* <p className="text-left text-base text-white font-bold mt-10">
+          {translate("contact.connect")}
+        </p> */}
       </div>
     </section>
   );
