1e679fb9f4ca36f82d1d093007bf6e2e94ae44b9
Luben Stoyanov <luben.stoyanov.ls@gmail.com>
2023-05-04 15:48:29 +0200

    Fix margin contact

--- components/contact.tsx
+++ components/contact.tsx
@@ -7,8 +7,8 @@ export default function Contact({ translate }: { translate: Function }) {
       id="contact"
       className="flex flex-col py-16 text-white bg-[#0D0E13]"
     >
-      <div className=" mx-7 lg:px-80">
-        <div className="flex place-items-center place-content-center gap-x-1">
+      <div className=" mx-7 lg:px-80 ">
+        <div className="flex place-items-center place-content-center gap-x-1 lg:px-20">
           <span className="text-xs font-bold">&lt;h2&gt; </span>
           <span className="border-t-2 w-20 lg:w-4/12 border-white"></span>
           <h2 className="font-bold lg:text-5xl text-3xl text-center">
@@ -17,8 +17,8 @@ export default function Contact({ translate }: { translate: Function }) {
           <span className="border-t-2 w-20 lg:w-4/12 border-white"></span>
           <span className="text-xs font-bold">&lt;h2/&gt;</span>
         </div>
-        <div className="lg:flex lg:justify-center">
-          <p className="pt-10  lg:max-w-[60ch">
+        <div className="lg:flex lg:flex-col lg:items-center">
+          <p className="pt-10  lg:max-w-[60ch]">
             <span className="text-xs font-semibold relative bottom-[2px]">
               &lt;p&gt;
             </span>{" "}
@@ -37,24 +37,24 @@ export default function Contact({ translate }: { translate: Function }) {
               &lt;/p&gt;
             </span>
           </p>
+          <button className="max-w-fit font-bold mt-10">
+            <a
+              href="mailto:luben.stoyanov.ls@gmail.com?subject=Inquiry"
+              className="no-underline"
+            >
+              <div className="flex">
+                <Image
+                  src={emailImage}
+                  alt="Mail couvert"
+                  className="w-8  self-center mr-1 shadow-lg transition delay-150 duration-300 ease-in-out hover:rotate-360 animate-pulse"
+                />
+                <span className="place-self-center italic  text-white">
+                  luben.stoyanov.ls@gmail.com
+                </span>
+              </div>
+            </a>
+          </button>
         </div>
-        <button className="max-w-fit font-bold mt-10">
-          <a
-            href="mailto:luben.stoyanov.ls@gmail.com?subject=Inquiry"
-            className="no-underline"
-          >
-            <div className="flex">
-              <Image
-                src={emailImage}
-                alt="Mail couvert"
-                className="w-8  self-center mr-1 shadow-lg transition delay-150 duration-300 ease-in-out hover:rotate-360 animate-pulse"
-              />
-              <span className="place-self-center italic  text-white">
-                luben.stoyanov.ls@gmail.com
-              </span>
-            </div>
-          </a>
-        </button>
         {/* <p className="text-left text-base text-white font-bold mt-10">
           {translate("contact.connect")}
         </p> */}
